d84b6b1da2d174ddca33f3d8f1aec8cea530b053
ERuvinga <ruvingaelie@gmail.com>
2025-03-29 07:55:52 +0200

    fix : add put methode

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUSH');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUSH, PUT');
     next();
 });
 
